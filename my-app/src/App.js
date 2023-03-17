358e72c0ddc380e6f32a0dbd8d6bf1dd8ece2026
mac <mac@MacBook-Pro-mac.local>
2023-03-17 19:58:34 +0300

    add styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
 // import { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 // import Counter from "./components/Counter";
+import "../src/styles/App.css";
+import PostItem from "./components/PostItem";
 
 function App() {
- 
-
   return (
-    <div>
-      {/* <Counter /> */}
-      <ClassCounter />
-      
+    <div className="App">
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
     </div>
   );
 }
