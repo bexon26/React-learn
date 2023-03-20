bd0e5a658b328280575273b9708d979c81d25499
mac <mac@MacBook-Pro-mac.local>
2023-03-20 21:42:52 +0300

    props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,22 @@ import ClassCounter from "./components/ClassCounter";
 // import Counter from "./components/Counter";
 import "../src/styles/App.css";
 import PostItem from "./components/PostItem";
+import { useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id:'1', title:"Javascript", body:'Язык программирования'},
+    {id:'2', title:"Javascript 2", body:'Язык программирования'},
+    {id:'3', title:"Javascript 3", body:'Язык программирования'},
+    {id:'4', title:"Javascript 4", body:'Язык программирования'}
+  ]);
   return (
     <div className="App">
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
+      {posts.map((post)=>
+        <PostItem post={post} />
+
+      )}
+      
     </div>
   );
 }
--- my-app/src/components/PostItem.jsx
+++ my-app/src/components/PostItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const PostItem = () => {
+const PostItem = (props) => {
   return (
     <div className="post">
       <div className="post__content">
-        <strong>1. Javascript</strong>
-        <div>Язык программирования</div>
+        <strong>{props.post.id} {props.post.title}</strong>
+        <div>{props.post.body}</div>
       </div>
       <div className="post__btns">
         <button>Удалить</button>
